07ca78c0761dc748931297b3d718ee05a3bc7d3c
stewart <stewart_robertson@sky.com>
2018-02-18 09:05:18 +0000

    set up debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'sl-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
